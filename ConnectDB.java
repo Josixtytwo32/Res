f67ee9b989ad29783137bbd77898779c67a762a9
Josixtytwo32 <99957215+Josixtytwo32@users.noreply.github.com>
2023-04-14 21:03:57 +0800

    Update ConnectDB.java

--- ConnectDB.java
+++ ConnectDB.java
@@ -10,7 +10,7 @@ public class ConnectDB{
     //Constructor
     public ConnectDB(){
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sql12600942","root","");
             st=con.createStatement();
             
@@ -31,7 +31,15 @@ public class ConnectDB{
             JOptionPane.showMessageDialog(null, "Error: "+ex);
         }
     }
-    
+    public void Transaclog (int TransactionCode, String ProductCode, String Category, String ItemDescription, int Quantity, float Price, float Total){
+        String query;
+        try{
+            query = "INSERT INTO 'transactionlog' ('Trancsaction Code', 'Product Code', 'Category', 'Item Description', ' Quantity', 'Price', 'Total')VALUES('"+TransactionCode+"', '"+ProductCode+"' , '"+Category+"', '"+ItemDescription+"', "+Quantity+", "+Price+", "+Total+")";
+            st.executeUpdate(query);
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error: "+ex);
+        }
+    }
     public static void main(String args[])
     {
         ConnectDB cdb=new ConnectDB();
